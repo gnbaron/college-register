f4c3b48b0f1e8d059be82e1e3aa66068bce19f6b
guilhermefloriani <floriani.guilherme@gmail.com>
2015-12-04 08:55:49 -0200

    Fix migration schema names.

--- lib/models/subject.ex
+++ lib/models/subject.ex
@@ -2,7 +2,7 @@ defmodule College.Subject do
   use Ecto.Schema
 
   @primary_key {:id, :id, autogenerate: true}
-  schema "subject" do
+  schema "subjects" do
     field :name, :string
     field :workload, :float, default: 0.0
     field :value,    :float, default: 0.0
--- priv/repo/migrations/20151204093235_migrations.exs
+++ priv/repo/migrations/20151204093235_migrations.exs
@@ -9,7 +9,7 @@ defmodule College.Repo.Migrations.Migrations do
   end
 
   defp subject_migration do
-    create table(:subject) do
+    create table(:subjects) do
       add :name, :string
       add :workload, :float, default: 0.0
       add :value, :float, default: 0.0
